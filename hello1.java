2ff3c477430fc524aaff9fafaaef8aab591b386e
navinrio <navinrio@gmail.com>
2023-04-24 21:14:37 +0900

    java code added

---
+++ hello1.java
@@ -0,0 +1,15 @@
+import java.lang.reflect.Method;
+
+class Baba {
+    int a = 10;
+    private void name() {
+        System.out.println("hello");
+        
+    }
+}
+
+class public static void main(String[] args) {
+    new nam
+    
+
+}
